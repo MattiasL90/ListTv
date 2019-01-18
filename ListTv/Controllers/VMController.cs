2821a2db6d6289d4e0996647f693f16b47444d10
MattiasL90 <mattias.liljegren@hotmail.se>
2019-01-18 11:36:56 +0100

    hyjy

---
+++ ListTv/Controllers/VMController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ListTv.Models;
+using ListTv.ViewModels;
+
+namespace ListTv.Controllers
+{
+    public class VMController : Controller
+    {
+        // GET: VM
+        public ActionResult Index()
+        {
+            DataBaseTvEntities dbtv = new DataBaseTvEntities();
+            List<ProgramVM> ProgramVMList = new List<ProgramVM>();
+            var proglist = (from cnl in dbtv.Channel
+                            join prg in dbtv.Program on cnl.Id equals prg.ChannelId
+                            select new { cnl.ChannelName, prg.ProgramName, prg.Time, prg.Date, prg.Length }).ToList();
+
+            foreach (var m in proglist)
+            {
+                ProgramVM pm = new ProgramVM();
+                pm.ChannelName = m.ChannelName;
+                pm.ProgramName = m.ProgramName;
+                pm.Time = m.Time;
+                pm.Date = m.Date;
+                pm.Length = m.Length;
+                ProgramVMList.Add(pm);
+            }
+            return View(ProgramVMList);
+        }
+    }
+}
---
+++ ListTv/ViewModels/ProgramVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListTv.ViewModels
+{
+    public class ProgramVM
+    {
+        public string ChannelName { get; set; }
+        public string ProgramName { get; set; }
+        public Nullable<System.TimeSpan> Time { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+        public Nullable<System.TimeSpan> Length { get; set; }
+    }
+}
